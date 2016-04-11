1dd99df6d67f2a4960d66b4d4ae8911a7841f67d
Tim Vaillancourt <tim@elementspace.com>
2016-04-11 11:34:05 +0200

    added upstream's improved version of my direct-connection fix for replsets, removed broken go tests to match upstream, pulled in a few bugfixes

--- collector/mongodb_collector.go
+++ collector/mongodb_collector.go
@@ -1,16 +1,12 @@
 package collector
 
 import (
-	//"github.com/dcu/mongodb_exporter/shared"
+	"github.com/dcu/mongodb_exporter/shared"
 	"github.com/dcu/mongodb_exporter/collector/mongod"
 	"github.com/dcu/mongodb_exporter/collector/mongos"
-	"github.com/dcu/mongodb_exporter/collector/shared"
 	"github.com/golang/glog"
 	"github.com/prometheus/client_golang/prometheus"
 	"gopkg.in/mgo.v2"
-	"fmt"
-	"os"
-        "regexp"
 )
 
 var (
@@ -40,146 +36,80 @@ func NewMongodbCollector(opts MongodbCollectorOpts) *MongodbCollector {
 // Describe describes all mongodb's metrics.
 func (exporter *MongodbCollector) Describe(ch chan<- *prometheus.Desc) {
 	glog.Info("Describing groups")
-	session, err := connectMongo(exporter.Opts.URI)
-    if err != nil{
-		return
-    }
-	serverStatus := collector_mongos.GetServerStatus(session)
-	if serverStatus != nil {
-		serverStatus.Describe(ch)
-	}
+	session := shared.MongoSession(exporter.Opts.URI)
 	defer session.Close()
-}
-
-func connectMongo(uri string)(*mgo.Session, error) {
-        r, _ := regexp.Compile("connect=direct")
-        if r.MatchString(uri) != true {
-            r, _ := regexp.Compile("\\?")
-            if r.MatchString(uri) == true {
-                uri = uri + "&connect=direct"
-            } else {
-                uri = uri + "?connect=direct"
-            }
-        }
-	session, err := mgo.Dial(uri)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "error: %v\n", err)
-        os.Exit(1)
-		glog.Errorf("Cannot connect to server using url: %s", uri)
-		return nil,err
+	if session != nil {
+		serverStatus := collector_mongos.GetServerStatus(session)
+		if serverStatus != nil {
+			serverStatus.Describe(ch)
+		}
 	}
-
-	session.SetMode(mgo.Eventual, true)
-  	session.SetSocketTimeout(0)
-
-	err = nil 
-	return session,err
-}
-
-// GetNodeType checks if the connected Session is a mongos, standalone, or replset,
-// by looking at the result of calling isMaster.
-func GetNodeType(session *mgo.Session)(string, error) {
-	masterDoc := struct {
-		SetName interface{} `bson:"setName"`
-		Hosts   interface{} `bson:"hosts"`
-		Msg     string      `bson:"msg"`
-	}{}
-	err := session.Run("isMaster", &masterDoc)
-	if err != nil {
-		glog.Info("Got unknown node type\n")
-		return "unknown", err
-	}
-
-	if masterDoc.SetName != nil || masterDoc.Hosts != nil {
-		glog.Info("Got replset node type")
-		return "replset", nil
-	} else if masterDoc.Msg == "isdbgrid" {
-		glog.Info("Got mongos node type\n")
-		// isdbgrid is always the msg value when calling isMaster on a mongos
-		// see http://docs.mongodb.org/manual/core/sharded-cluster-query-router/
-		return "mongos", nil
-	}
-	glog.Info("defaulted to mongod node type\n")
-	return "mongod", nil
 }
 
 // Collect collects all mongodb's metrics.
 func (exporter *MongodbCollector) Collect(ch chan<- prometheus.Metric) {
-    glog.Info("Collecting Server Status from: ", exporter.Opts.URI)
-    session, err := connectMongo(exporter.Opts.URI)
-    if err != nil{
-		 glog.Error(fmt.Printf("We failed to connect to mongo with error of %s\n", err))
-    }
+	mongoSess := shared.MongoSession(exporter.Opts.URI)
+	defer mongoSess.Close()
+	if mongoSess != nil {
+		serverVersion, err := shared.MongoSessionServerVersion(mongoSess)
+		if err != nil {
+			glog.Errorf("Problem gathering the mongo server version: %s", err)
+		}
 
-    version := collector_shared.GetServerVersion(session)
-    glog.Info("Connected to: ", exporter.Opts.URI, ", server version: ", version)
+		nodeType, err := shared.MongoSessionNodeType(mongoSess)
+		if err != nil {
+			glog.Errorf("Problem gathering the mongo node type: %s", err)
+		}
 
-    nodeType,err := GetNodeType(session)
-    if err != nil{
-    	glog.Error("We run had a node type error of %s\n", err)
-    }
-    //glog.Info("Passed nodeType with %s", nodeType)
-    switch {
-    	case nodeType == "mongos":
-    	    serverStatus := collector_mongos.GetServerStatus(session)
-    	    if serverStatus != nil {
-	        serverStatus.Export(ch)
-	    }
-            balancerStatus := collector_mongos.GetBalancerStatus(session)
-            if balancerStatus != nil {
-                balancerStatus.Export(ch)
-            }
-            balancerTopoStatus := collector_mongos.GetBalancerTopoStatus(session)
-            if balancerTopoStatus != nil {
-                balancerTopoStatus.Export(ch)
-            }
-            balancerChangelogStatus := collector_mongos.GetBalancerChangelogStatus(session)
-            if balancerChangelogStatus != nil {
-                balancerChangelogStatus.Export(ch)
-            }
-        case nodeType == "mongod":
-	    serverStatus := collector_mongod.GetServerStatus(session)
-    	    if serverStatus != nil {
-	        serverStatus.Export(ch)
-	    }
-        case nodeType == "replset":
-	    serverStatus := collector_mongod.GetServerStatus(session)
-    	    if serverStatus != nil {
-	        serverStatus.Export(ch)
-	    }
-            oplogStatus := collector_mongod.GetOplogStatus(session)
-            if oplogStatus != nil {
-                oplogStatus.Export(ch)
-            }
-            replSetStatus := collector_mongod.GetReplSetStatus(session)
-            if replSetStatus != nil {
-                replSetStatus.Export(ch)
-            }
-	default:
-	    glog.Info("No process for current node type no metrics printing!\n")
-    }
-    session.Close()
-    //exporter.collectMongodServerStatus(ch)
-    //exporter.collectMongosServerStatus(ch)
+		glog.Infof("Connected to: %s (node type: %s, server version: %s)", exporter.Opts.URI, nodeType, serverVersion)
+		switch {
+			case nodeType == "mongos":
+				exporter.collectMongos(mongoSess, ch)
+			case nodeType == "mongod":
+				exporter.collectMongod(mongoSess, ch)
+			case nodeType == "replset":
+				exporter.collectMongodReplSet(mongoSess, ch)
+			default:
+				glog.Infof("Unrecognized node type %s!", nodeType)
+		}
+	}
 }
-/**
-func (exporter *MongodbCollector) collectMongodServerStatus(ch chan<- prometheus.Metric) *collector_mongod.ServerStatus {
-	serverStatus := collector_mongod.GetServerStatus(exporter.Opts.URI)
 
+func (exporter *MongodbCollector) collectMongos(session *mgo.Session, ch chan<- prometheus.Metric) {
+	glog.Info("Collecting Server Status")
+	serverStatus := collector_mongos.GetServerStatus(session)
 	if serverStatus != nil {
 		serverStatus.Export(ch)
 	}
 
-	return serverStatus
+	glog.Info("Collecting Mongos Balancer Status")
+	balancerStatus := collector_mongos.GetBalancerStatus(session)
+	if balancerStatus != nil {
+		balancerStatus.Export(ch)
+	}
 }
 
-func (exporter *MongodbCollector) collectMongosServerStatus(ch chan<- prometheus.Metric) *collector_mongos.ServerStatus {
-	serverStatus := collector_mongos.GetServerStatus(exporter.Opts.URI)
-
+func (exporter *MongodbCollector) collectMongod(session *mgo.Session, ch chan<- prometheus.Metric) {
+	glog.Info("Collecting Server Status")
+	serverStatus := collector_mongod.GetServerStatus(session)
 	if serverStatus != nil {
 		serverStatus.Export(ch)
 	}
-
-	return serverStatus
 }
-**/
+
+func (exporter *MongodbCollector) collectMongodReplSet(session *mgo.Session, ch chan<- prometheus.Metric) {
+	exporter.collectMongod(session, ch)
+
+	glog.Info("Collecting Replset Status")
+	oplogStatus := collector_mongod.GetOplogStatus(session)
+	if oplogStatus != nil {
+		oplogStatus.Export(ch)
+	}       
+
+	glog.Info("Collecting Replset Oplog Status")
+	replSetStatus := collector_mongod.GetReplSetStatus(session)
+	if replSetStatus != nil {
+		replSetStatus.Export(ch)
+	}       
+}
+
--- mongodb_exporter.go
+++ mongodb_exporter.go
@@ -3,21 +3,30 @@ package main
 import (
 	"flag"
 	"fmt"
+	"net/http"
+	"os"
+
 	"github.com/dcu/mongodb_exporter/collector"
 	"github.com/dcu/mongodb_exporter/shared"
+
 	"github.com/prometheus/client_golang/prometheus"
-	"net/http"
 )
+
+func mongodbDefaultUri() string {
+	if u := os.Getenv("MONGODB_URL"); u != "" {
+		return u
+	}
+	return "mongodb://localhost:27017"
+}
 
 var (
 	listenAddressFlag = flag.String("web.listen-address", ":9001", "Address on which to expose metrics and web interface.")
 	metricsPathFlag   = flag.String("web.metrics-path", "/metrics", "Path under which to expose metrics.")
 
-	mongodbURIFlag    = flag.String("mongodb.uri", "mongodb://localhost:27017", "Mongodb URI, format: [mongodb://][user:pass@]host1[:port1][,host2[:port2],...][/database][?options]")
+	mongodbURIFlag    = flag.String("mongodb.uri", mongodbDefaultUri(), "Mongodb URI, format: [mongodb://][user:pass@]host1[:port1][,host2[:port2],...][/database][?options]")
 	enabledGroupsFlag = flag.String("groups.enabled", "asserts,durability,background_flushing,connections,extra_info,global_lock,index_counters,network,op_counters,op_counters_repl,memory,locks,metrics", "Comma-separated list of groups to use, for more info see: docs.mongodb.org/manual/reference/command/serverStatus/")
-	//printCollectors   = flag.Bool("collectors.print", false, "If true, print available collectors and exit.")
-	authUserFlag = flag.String("auth.user", "", "Username for basic auth.")
-	authPassFlag = flag.String("auth.pass", "", "Password for basic auth.")
+	authUserFlag      = flag.String("auth.user", "", "Username for basic auth.")
+	authPassFlag      = flag.String("auth.pass", "", "Password for basic auth.")
 )
 
 type basicAuthHandler struct {
