0aa986ac7adc38390e71313002ebf948612591b1
Tim Vaillancourt <tim@elementspace.com>
2016-08-18 15:07:50 +0200

    process inMemory stats as WiredTiger

--- collector/mongod/server_status.go
+++ collector/mongod/server_status.go
@@ -61,6 +61,7 @@ type ServerStatus struct {
 
 	Cursors *Cursors `bson:"cursors"`
 
+	InMemory	*WiredTigerStats	`bson:"inMemory"`
 	RocksDb		*RocksDbStats		`bson:"rocksdb"`
 	WiredTiger	*WiredTigerStats	`bson:"wiredTiger"`
 }
@@ -115,6 +116,9 @@ func (status *ServerStatus) Export(ch chan<- prometheus.Metric) {
 	}
 	if status.Cursors != nil {
 		status.Cursors.Export(ch)
+	}
+	if status.InMemory != nil {
+		status.InMemory.Export(ch)
 	}
 	if status.RocksDb != nil {
 		status.RocksDb.Export(ch)
@@ -171,6 +175,9 @@ func (status *ServerStatus) Describe(ch chan<- *prometheus.Desc) {
 	}
 	if status.Cursors != nil {
 		status.Cursors.Describe(ch)
+	}
+	if status.InMemory != nil {
+		status.InMemory.Describe(ch)
 	}
 	if status.RocksDb != nil {
 		status.RocksDb.Describe(ch)
