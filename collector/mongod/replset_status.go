0f090e1e247e35b9d02e556682d6346bfa749ce1
Tim Vaillancourt <tim@elementspace.com>
2016-11-20 21:59:49 +0100

    fix label order to match

--- collector/mongod/replset_status.go
+++ collector/mongod/replset_status.go
@@ -17,7 +17,7 @@ var (
 		Subsystem: subsystem,
 		Name:      "my_name",
 		Help:      "The replica state name of the current member",
-	}, []string{"name", "set"})
+	}, []string{"set", "name"})
 	myState   = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Namespace: Namespace,
 		Subsystem: subsystem,
