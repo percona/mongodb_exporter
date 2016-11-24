a54a15902cae90c7fa879ccde57e063f193bce00
Roman Vynar <roman.vynar@percona.com>
2016-11-24 18:38:02 +0200

    Added -web.auth-file flag for http basic auth setup.

--- mongodb_exporter.go
+++ mongodb_exporter.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"strings"
@@ -11,6 +12,7 @@ import (
 	"github.com/Percona-Lab/prometheus_mongodb_exporter/shared"
 
 	"github.com/prometheus/client_golang/prometheus"
+	"gopkg.in/yaml.v2"
 )
 
 func mongodbDefaultUri() string {
@@ -27,6 +29,7 @@ var (
 	doPrintVersion    = flag.Bool("version", false, "Print version info and exit.")
 	listenAddressFlag = flag.String("web.listen-address", ":9104", "Address on which to expose metrics and web interface.")
 	metricsPathFlag   = flag.String("web.metrics-path", "/metrics", "Path under which to expose metrics.")
+	webAuthFile       = flag.String("web.auth-file", "", "Path to YAML file with server_user, server_password options for http basic auth (overrides HTTP_AUTH env var).")
 
 	mongodbURIFlag    = flag.String("mongodb.uri", mongodbDefaultUri(), "Mongodb URI, format: [mongodb://][user:pass@]host1[:port1][,host2[:port2],...][/database][?options]")
 	enabledGroupsFlag = flag.String("groups.enabled", "asserts,durability,background_flushing,connections,extra_info,global_lock,index_counters,network,op_counters,op_counters_repl,memory,locks,metrics", "Comma-separated list of groups to use, for more info see: docs.mongodb.org/manual/reference/command/serverStatus/")
@@ -34,6 +37,11 @@ var (
 
 func printVersion() {
 	fmt.Printf("mongodb_exporter version: %s, git commit hash: %s\n", version, versionGitCommit)
+}
+
+type webAuth struct {
+	User     string `yaml:"server_user,omitempty"`
+	Password string `yaml:"server_password,omitempty"`
 }
 
 type basicAuthHandler struct {
@@ -54,25 +62,29 @@ func (h *basicAuthHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func prometheusHandler() http.Handler {
-	var authUser, authPass string
+	cfg := &webAuth{}
 	httpAuth := os.Getenv("HTTP_AUTH")
-	if httpAuth != "" {
+	if *webAuthFile != "" {
+		bytes, err := ioutil.ReadFile(*webAuthFile)
+		if err != nil {
+			panic(fmt.Sprintf("Cannot read auth file: %s", err))
+		}
+		if err := yaml.Unmarshal(bytes, cfg); err != nil {
+			panic(fmt.Sprintf("Cannot parse auth file: %s", err))
+		}
+	} else if httpAuth != "" {
 		data := strings.SplitN(httpAuth, ":", 2)
 		if len(data) != 2 || data[0] == "" || data[1] == "" {
 			panic("HTTP_AUTH should be formatted as user:password")
 		}
-		authUser = data[0]
-		authPass = data[1]
-		fmt.Println("HTTP basic authentication is enabled")
+		cfg.User = data[0]
+		cfg.Password = data[1]
 	}
 
 	handler := prometheus.Handler()
-	if authUser != "" &&  authPass != "" {
-		handler = &basicAuthHandler{
-			handler:  handler.ServeHTTP,
-			user:     authUser,
-			password: authPass,
-		}
+	if cfg.User != "" && cfg.Password != "" {
+		handler = &basicAuthHandler{handler: handler.ServeHTTP, user: cfg.User, password: cfg.Password}
+		fmt.Println("HTTP basic authentication is enabled")
 	}
 
 	return handler
