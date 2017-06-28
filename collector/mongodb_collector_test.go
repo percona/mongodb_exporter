caaf722cc064988635df941996863fc5c89986c5
Alexey Palazhchenko <alexey.palazhchenko@percona.com>
2017-06-28 16:38:19 +0300

    Use Mongo in Docker in integration tests.

--- collector/mongodb_collector_test.go
+++ collector/mongodb_collector_test.go
@@ -9,20 +9,32 @@ import (
 )
 
 func TestCollectServerStatus(t *testing.T) {
+	if testing.Short() {
+		t.Skip("-short is passed, skipping integration test")
+	}
+
 	shared.ParseEnabledGroups("assers,durability,backgrond_flushing,connections,extra_info,global_lock,index_counters,network,op_counters,memory,locks,metrics,cursors")
-	collector := NewMongodbCollector(MongodbCollectorOpts{URI: "localhost"})
+	collector := NewMongodbCollector(MongodbCollectorOpts{URI: "mongodb://localhost:27017"})
 	go collector.Collect(nil)
 }
 
 func TestDescribeCollector(t *testing.T) {
-	collector := NewMongodbCollector(MongodbCollectorOpts{URI: "localhost"})
+	if testing.Short() {
+		t.Skip("-short is passed, skipping integration test")
+	}
+
+	collector := NewMongodbCollector(MongodbCollectorOpts{URI: "mongodb://localhost:27017"})
 
 	ch := make(chan *prometheus.Desc)
 	go collector.Describe(ch)
 }
 
 func TestCollectCollector(t *testing.T) {
-	collector := NewMongodbCollector(MongodbCollectorOpts{URI: "localhost"})
+	if testing.Short() {
+		t.Skip("-short is passed, skipping integration test")
+	}
+
+	collector := NewMongodbCollector(MongodbCollectorOpts{URI: "mongodb://localhost:27017"})
 
 	ch := make(chan prometheus.Metric)
 	go collector.Collect(ch)
