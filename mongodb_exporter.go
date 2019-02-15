368eaa46b1ae0995ec9f941a7aa5c3b60b8f77cb
Anton Kucherov <anton.kucherov@percona.com>
2019-02-15 10:25:44 +0300

    PMM-3512: default values was recovered for some flags

--- mongodb_exporter.go
+++ mongodb_exporter.go
@@ -54,11 +54,11 @@ var (
 	tlsF     = kingpin.Flag("mongodb.tls", "Enable tls connection with mongo server").Bool()
 	tlsCertF = kingpin.Flag("mongodb.tls-cert", "Path to PEM file that contains the certificate (and optionally also the decrypted private key in PEM format).\n"+
 		"    \tThis should include the whole certificate chain.\n"+
-		"    \tIf provided: The connection will be opened via TLS to the MongoDB server.").String()
-	tlsPrivateKeyF = kingpin.Flag("mongodb.tls-private-key", "Path to PEM file that contains the decrypted private key (if not contained in mongodb.tls-cert file).").String()
+		"    \tIf provided: The connection will be opened via TLS to the MongoDB server.").Default("").String()
+	tlsPrivateKeyF = kingpin.Flag("mongodb.tls-private-key", "Path to PEM file that contains the decrypted private key (if not contained in mongodb.tls-cert file).").Default("").String()
 	tlsCAF         = kingpin.Flag("mongodb.tls-ca", "Path to PEM file that contains the CAs that are trusted for server connections.\n"+
 		"    \tIf provided: MongoDB servers connecting to should present a certificate signed by one of this CAs.\n"+
-		"    \tIf not provided: System default CAs are used.").String()
+		"    \tIf not provided: System default CAs are used.").Default("").String()
 	tlsDisableHostnameValidationF = kingpin.Flag("mongodb.tls-disable-hostname-validation", "Disable hostname validation for server connection.").Bool()
 	maxConnectionsF               = kingpin.Flag("mongodb.max-connections", "Max number of pooled connections to the database.").Default("1").Int()
 	testF                         = kingpin.Flag("test", "Check MongoDB connection, print buildInfo() information and exit.").Bool()
@@ -71,7 +71,7 @@ var (
 
 	// FIXME currently ignored
 	// enabledGroupsFlag = flag.String("groups.enabled", "asserts,durability,background_flushing,connections,extra_info,global_lock,index_counters,network,op_counters,op_counters_repl,memory,locks,metrics", "Comma-separated list of groups to use, for more info see: docs.mongodb.org/manual/reference/command/serverStatus/")
-	enabledGroupsFlag = kingpin.Flag("groups.enabled", "Currently ignored").String()
+	enabledGroupsFlag = kingpin.Flag("groups.enabled", "Currently ignored").Default("").String()
 )
 
 func main() {
