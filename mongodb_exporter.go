ebded764696d50568e75469d74b3841f84f51190
Anton Kucherov <anton.kucherov@percona.com>
2019-06-10 14:35:03 +0300

    PMM-4154 Add log-levels.

--- mongodb_exporter.go
+++ mongodb_exporter.go
@@ -38,47 +38,49 @@ const (
 )
 
 var (
-	listenAddressF = kingpin.Flag("web.listen-address", "Address to listen on for web interface and telemetry.").Default(":9216").String()
-	metricsPathF   = kingpin.Flag("web.telemetry-path", "Path under which to expose metrics.").Default("/metrics").String()
+	app            = kingpin.New(program, "")
+	listenAddressF = app.Flag("web.listen-address", "Address to listen on for web interface and telemetry.").Default(":9216").String()
+	metricsPathF   = app.Flag("web.telemetry-path", "Path under which to expose metrics.").Default("/metrics").String()
 
-	collectDatabaseF   = kingpin.Flag("collect.database", "Enable collection of Database metrics").Bool()
-	collectCollectionF = kingpin.Flag("collect.collection", "Enable collection of Collection metrics").Bool()
-	collectTopF        = kingpin.Flag("collect.topmetrics", "Enable collection of table top metrics").Bool()
-	collectIndexUsageF = kingpin.Flag("collect.indexusage", "Enable collection of per index usage stats").Bool()
+	collectDatabaseF   = app.Flag("collect.database", "Enable collection of Database metrics").Bool()
+	collectCollectionF = app.Flag("collect.collection", "Enable collection of Collection metrics").Bool()
+	collectTopF        = app.Flag("collect.topmetrics", "Enable collection of table top metrics").Bool()
+	collectIndexUsageF = app.Flag("collect.indexusage", "Enable collection of per index usage stats").Bool()
 
-	uriF = kingpin.Flag("mongodb.uri", "MongoDB URI, format").
+	uriF = app.Flag("mongodb.uri", "MongoDB URI, format").
 		PlaceHolder("[mongodb://][user:pass@]host1[:port1][,host2[:port2],...][/database][?options]").
 		Default("mongodb://localhost:27017").
 		Envar("MONGODB_URI").
 		String()
 
-	authDB   = kingpin.Flag("mongodb.authentification-database", "Specifies the database in which the user is created").Default("").String()
-	tlsF     = kingpin.Flag("mongodb.tls", "Enable tls connection with mongo server").Bool()
-	tlsCertF = kingpin.Flag("mongodb.tls-cert", "Path to PEM file that contains the certificate (and optionally also the decrypted private key in PEM format).\n"+
+	authDB   = app.Flag("mongodb.authentification-database", "Specifies the database in which the user is created").Default("").String()
+	tlsF     = app.Flag("mongodb.tls", "Enable tls connection with mongo server").Bool()
+	tlsCertF = app.Flag("mongodb.tls-cert", "Path to PEM file that contains the certificate (and optionally also the decrypted private key in PEM format).\n"+
 		"    \tThis should include the whole certificate chain.\n"+
 		"    \tIf provided: The connection will be opened via TLS to the MongoDB server.").Default("").String()
-	tlsPrivateKeyF = kingpin.Flag("mongodb.tls-private-key", "Path to PEM file that contains the decrypted private key (if not contained in mongodb.tls-cert file).").Default("").String()
-	tlsCAF         = kingpin.Flag("mongodb.tls-ca", "Path to PEM file that contains the CAs that are trusted for server connections.\n"+
+	tlsPrivateKeyF = app.Flag("mongodb.tls-private-key", "Path to PEM file that contains the decrypted private key (if not contained in mongodb.tls-cert file).").Default("").String()
+	tlsCAF         = app.Flag("mongodb.tls-ca", "Path to PEM file that contains the CAs that are trusted for server connections.\n"+
 		"    \tIf provided: MongoDB servers connecting to should present a certificate signed by one of this CAs.\n"+
 		"    \tIf not provided: System default CAs are used.").Default("").String()
-	tlsDisableHostnameValidationF = kingpin.Flag("mongodb.tls-disable-hostname-validation", "Disable hostname validation for server connection.").Bool()
-	maxConnectionsF               = kingpin.Flag("mongodb.max-connections", "Max number of pooled connections to the database.").Default("1").Int()
-	testF                         = kingpin.Flag("test", "Check MongoDB connection, print buildInfo() information and exit.").Bool()
+	tlsDisableHostnameValidationF = app.Flag("mongodb.tls-disable-hostname-validation", "Disable hostname validation for server connection.").Bool()
+	maxConnectionsF               = app.Flag("mongodb.max-connections", "Max number of pooled connections to the database.").Default("1").Int()
+	testF                         = app.Flag("test", "Check MongoDB connection, print buildInfo() information and exit.").Bool()
 
-	socketTimeoutF = kingpin.Flag("mongodb.socket-timeout", "Amount of time to wait for a non-responding socket to the database before it is forcefully closed.\n"+
+	socketTimeoutF = app.Flag("mongodb.socket-timeout", "Amount of time to wait for a non-responding socket to the database before it is forcefully closed.\n"+
 		"    \tValid time units are 'ns', 'us' (or 'µs'), 'ms', 's', 'm', 'h'.").Default("3s").Duration()
-	syncTimeoutF = kingpin.Flag("mongodb.sync-timeout", "Amount of time an operation with this session will wait before returning an error in case\n"+
+	syncTimeoutF = app.Flag("mongodb.sync-timeout", "Amount of time an operation with this session will wait before returning an error in case\n"+
 		"    \ta connection to a usable server can't be established.\n"+
 		"    \tValid time units are 'ns', 'us' (or 'µs'), 'ms', 's', 'm', 'h'.").Default("1m").Duration()
 
 	// FIXME currently ignored
 	// enabledGroupsFlag = flag.String("groups.enabled", "asserts,durability,background_flushing,connections,extra_info,global_lock,index_counters,network,op_counters,op_counters_repl,memory,locks,metrics", "Comma-separated list of groups to use, for more info see: docs.mongodb.org/manual/reference/command/serverStatus/")
-	enabledGroupsFlag = kingpin.Flag("groups.enabled", "Currently ignored").Default("").String()
+	enabledGroupsFlag = app.Flag("groups.enabled", "Currently ignored").Default("").String()
 )
 
 func main() {
-	initVersionInfo()
-	kingpin.Parse()
+	initVersionInfo(app)
+	log.AddFlags(app)
+	kingpin.MustParse(app.Parse(os.Args[1:]))
 
 	if *testF {
 		buildInfo, err := shared.TestConnection(
@@ -133,7 +135,8 @@ func main() {
 // to application version and will be printed in all logs.
 // TODO: Refactor after moving version.Info() and version.BuildContext() to https://github.com/percona/exporter_shared
 // See: https://jira.percona.com/browse/PMM-3250 and https://github.com/percona/mongodb_exporter/pull/132#discussion_r262227248
-func initVersionInfo() {
+func initVersionInfo(app *kingpin.Application) {
+
 	version.Version = pmmVersion.Version
 	version.Revision = pmmVersion.FullCommit
 	version.Branch = pmmVersion.Branch
@@ -146,11 +149,11 @@ func initVersionInfo() {
 
 	if pmmVersion.PMMVersion != "" {
 		version.Version += "-pmm-" + pmmVersion.PMMVersion
-		kingpin.Version(pmmVersion.FullInfo())
+		app.Version(pmmVersion.FullInfo())
 	} else {
-		kingpin.Version(version.Print(program))
+		app.Version(version.Print(program))
 	}
 
-	kingpin.HelpFlag.Short('h')
-	kingpin.CommandLine.Help = fmt.Sprintf("%s exports various MongoDB metrics in Prometheus format.\n", pmmVersion.ShortInfo())
+	app.HelpFlag.Short('h')
+	app.Help = fmt.Sprintf("%s exports various MongoDB metrics in Prometheus format.\n", pmmVersion.ShortInfo())
 }
