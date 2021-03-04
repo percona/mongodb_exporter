162eb5d67d939eca4e73070d32a636eed2aaa0a9
Denys Kondratenko <denisok@users.noreply.github.com>
2021-03-04 18:49:13 +0100

    PMM-7643 add release action (#227)
    Add release action that is based on goreleaser that is already present.
    
    Fix action location to the percona-platform.
    
    Deprecating set-env and add-path
    
    Use Go 1.17 gofmt format
    https://go.googlesource.com/proposal/+/master/design/draft-gobuild.md
    otherwise diff check would fail as gofmt adds additional lines

--- .github/check-license.go
+++ .github/check-license.go
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+//go:build ignore
 // +build ignore
 
 // check-license checks that AGPL license header in all files matches header in this file.
