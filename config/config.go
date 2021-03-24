5532d1c69773f8edbea93d7cce40abd72422c912
jingningdeng <1615103105@qq.com>
2021-03-24 18:16:16 +0800

    perf():多目标采集

--- config/config.go
+++ config/config.go
@@ -3,12 +3,14 @@ package config
 type Config struct {
 	MongoInstance []*MongoInstance `yaml:"mongo-list",json:"mongo-list"`
 }
+
 type MongoInstance struct {
-	Name string `yaml:"name",json:"name"`
-	Host string `yaml:"host",json:"host"`
-	Port string `yaml:"port",json:"port"`
+	Name    string     `yaml:"name",json:"name"`
+	Host    string     `yaml:"host",json:"host"`
+	Port    string     `yaml:"port",json:"port"`
 	Account []*Account `yaml:"account",json:"account"`
 }
+
 type Account struct {
 	Username string `yaml:"username",json:"username"`
 	Password string `yaml:"password",json:"password"`
