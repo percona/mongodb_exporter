b407320c7fa0648f9230d81e57bb26e163b7e4e1
Nurlan Moldomurov <nurlan.moldomurov@percona.com>
2022-01-27 17:43:57 +0300

    Update common.go

--- exporter/common.go
+++ exporter/common.go
@@ -167,7 +167,6 @@ func listAllCollections(ctx context.Context, client *mongo.Client, filterInNames
 		filterNS = append(filterNS, dbs...)
 	}
 
-
 	for _, db := range dbs {
 		for _, namespace := range filterNS {
 			parts := strings.Split(namespace, ".")
