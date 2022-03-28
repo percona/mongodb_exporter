689d7df9a91ac3f35395c63aa0d7fc18a6f6efbd
Carlos Salguero <carlos.salguero@percona.com>
2022-03-28 09:07:07 -0300

    Format

--- exporter/top_collector.go
+++ exporter/top_collector.go
@@ -38,7 +38,8 @@ type topCollector struct {
 var ErrInvalidOrMissingTotalsEntry = fmt.Errorf("Invalid or misssing totals entry in top results")
 
 func newTopCollector(ctx context.Context, client *mongo.Client, logger *logrus.Logger, compatible bool,
-	topology labelsGetter) *topCollector {
+	topology labelsGetter,
+) *topCollector {
 	return &topCollector{
 		ctx:            ctx,
 		base:           newBaseCollector(client, logger),
