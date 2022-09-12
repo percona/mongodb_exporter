a226795c505de0818320fcc8e68f0c1f1d11b8f8
Trevor North <trevor@bugsnag.com>
2022-09-12 16:43:07 +0100

    Fix test expectations

--- exporter/serverstatus_collector_test.go
+++ exporter/serverstatus_collector_test.go
@@ -41,19 +41,19 @@ func TestServerStatusDataCollector(t *testing.T) {
 
 	// The last \n at the end of this string is important
 	expected := strings.NewReader(`
-	# HELP mongodb_mem_bits mem.
-	# TYPE mongodb_mem_bits untyped
-	mongodb_mem_bits 64
-	# HELP mongodb_metrics_commands_connPoolSync_failed metrics.commands.connPoolSync.
-	# TYPE mongodb_metrics_commands_connPoolSync_failed untyped
-	mongodb_metrics_commands_connPoolSync_failed 0` + "\n")
+	# HELP mongodb_ss_mem_bits serverStatus.mem.
+	# TYPE mongodb_ss_mem_bits untyped
+	mongodb_ss_mem_bits 64
+	# HELP mongodb_ss_metrics_commands_connPoolSync_failed serverStatus.metrics.commands.connPoolSync.
+	# TYPE mongodb_ss_metrics_commands_connPoolSync_failed untyped
+	mongodb_ss_metrics_commands_connPoolSync_failed 0` + "\n")
 	// Filter metrics for 2 reasons:
 	// 1. The result is huge
 	// 2. We need to check against know values. Don't use metrics that return counters like uptime
 	//    or counters like the number of transactions because they won't return a known value to compare
 	filter := []string{
-		"mongodb_mem_bits",
-		"mongodb_metrics_commands_connPoolSync_failed",
+		"mongodb_ss_mem_bits",
+		"mongodb_ss_metrics_commands_connPoolSync_failed",
 	}
 	err := testutil.CollectAndCompare(c, expected, filter...)
 	assert.NoError(t, err)
