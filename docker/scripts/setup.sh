20c1a986787912161ebaf226b10285cf808110ab
Jiří Čtvrtka <j.ctvrtka@gmail.com>
2024-01-05 09:42:55 +0100

    PMM-12712 Michael's fix.

--- docker/scripts/setup.sh
+++ docker/scripts/setup.sh
@@ -1,14 +1,5 @@
 #!/bin/bash 
 
-readarray -d . -t verarr <<< "${VERSION}"
-readarray -d : -t version <<< "${verarr[0]}"
-echo "Mongo version: ${version[1]}"
-
-if ((${version[1]} <= 4))
-then
-  echo -e 'mongo' > /usr/bin/mongosh && chmod +x /usr/bin/mongosh
-fi
-
 mongodb1=`getent hosts ${MONGO1} | awk '{ print $1 }'`
 mongodb2=`getent hosts ${MONGO2} | awk '{ print $1 }'`
 mongodb3=`getent hosts ${MONGO3} | awk '{ print $1 }'`
@@ -17,7 +8,7 @@ arbiter=`getent hosts ${ARBITER} | awk '{ print $1 }'`
 port=${PORT:-27017}
 
 echo "Waiting for startup.."
-until mongosh --host ${mongodb1}:${port} --eval 'quit(db.runCommand({ ping: 1 }).ok ? 0 : 2)' &>/dev/null; do
+until mongo --host ${mongodb1}:${port} --eval 'quit(db.runCommand({ ping: 1 }).ok ? 0 : 2)' &>/dev/null; do
   printf '.'
   sleep 1
 done
@@ -28,10 +19,11 @@ echo setup.sh time now: `date +"%T" `
 
 
 function cnf_servers() {
-    echo "setup cnf servers"
-    mongosh --host ${mongodb1}:${port} <<EOF
+    echo "setup cnf servers on ${MONGO1}(${mongodb1}:${port})"
+    mongo --host ${mongodb1}:${port} <<EOF
     var cfg = {
         "_id": "${RS}",
+        "version": 1,
         "protocolVersion": 1,
         "configsvr": true,
         "members": [
@@ -46,20 +38,21 @@ function cnf_servers() {
             {
                 "_id": 2,
                 "host": "${mongodb3}:${port}"
-            }
+            },
         ]
     };
-    rs.initiate(cfg, { force: true });
-    rs.reconfig(cfg, { force: true });
+
+    rs.initiate(cfg);
 EOF
 }
 
 function general_servers() {
-    echo "setup servers"
-    mongosh --host ${mongodb1}:${port} <<EOF
+    echo "setup servers on ${MONGO1}(${mongodb1}:${port})"
+    mongo --host ${mongodb1}:${port} <<EOF
     var cfg = {
         "_id": "${RS}",
         "protocolVersion": 1,
+        "version": 1,
         "members": [
             {
                 "_id": 0,
@@ -72,13 +65,15 @@ function general_servers() {
             {
                 "_id": 2,
                 "host": "${mongodb3}:${port}"
-            }
+            },
+            {
+                "_id": 3,
+                "host": "${arbiter}:${port}",
+                "arbiterOnly": true
+            },
         ]
     };
-    rs.initiate(cfg, { force: true });
-    rs.reconfig(cfg, { force: true });
-
-    rs.addArb("${arbiter}:${port}")
+    rs.initiate(cfg);
 EOF
 }
 
@@ -87,6 +82,11 @@ case $1 in
         cnf_servers
         shift
         ;;
+    *)
+        general_servers
+        shift
+        ;;
+esac
     *)
         general_servers
         shift
