8cbd642fd326b6b9423ef362491e9f141a295967
idoko <okokomichaels@outlook.com>
2024-06-25 07:43:18 +0100

    fix set up scripts for tests

--- docker/scripts/setup-pbm.sh
+++ docker/scripts/setup-pbm.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 sleep 3
+
+# We use IP address for the replica set since the docker host name will not be resolvable on the host during tests.
+PBM_MONGODB_HOST=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' standalone-backup)
+
 docker exec standalone-backup bash -c "
 mongo 'mongodb://pbm:pbm@localhost' <<EOF
 rs.initiate(
@@ -8,7 +12,7 @@ rs.initiate(
         version: 1,
         members: [
 
-            { _id: 0, \"host\": \"standalone-backup:27017\" }
+            { _id: 0, \"host\": \"${PBM_MONGODB_HOST}:27017\" }
         ]
     }
 )
