382894518ffdcdcbe6e8b93653c159d01d8c7d26
idoko <okokomichaels@outlook.com>
2025-03-05 07:08:57 +0100

    create separate exporter keytab

--- docker/scripts/setup-krb5-mongo.sh
+++ docker/scripts/setup-krb5-mongo.sh
@@ -8,4 +8,5 @@ docker exec ${KERBEROS_HOST} bash -c "kinit pmm-test@PERCONATEST.COM -kt /tmp/mo
 
 #docker exec --user root ${MONGO_HOST} bash -c "chown -R mongodb:root /tmp/krb5cc_0"
 docker exec --user root ${MONGO_HOST} bash -c "chown -R mongodb:root /tmp/mongodb.keytab"
-docker exec ${MONGO_HOST} mongosh "${MONGO_HOST}:${port}" -u ${username} -p ${password} --eval 'db.getSiblingDB("$external").createUser({user: "pmm-test@PERCONATEST.COM",roles: [{role: "read", db: "admin"}]});'
+docker exec ${MONGO_HOST} mongosh "${MONGO_HOST}:${port}" -u ${username} -p ${password} --eval ''
+db.getSiblingDB("$external").createUser({user: "pmm-test@PERCONATEST.COM",roles: [{role: "read", db: "admin"}]});
--- docker/scripts/setup-krb5-server.sh
+++ docker/scripts/setup-krb5-server.sh
@@ -2,6 +2,7 @@
 
 mongohost=`getent hosts ${MONGO_HOST} | awk '{ print $1 }'`
 kerberos_host=`getent hosts ${KERBEROS_HOST} | awk '{ print $1 }'`
+gateway_ip=`ip route | grep default | awk '{print $3}'`
 
 cat > /etc/krb5.conf <<EOL
 [libdefaults]
@@ -11,6 +12,7 @@ cat > /etc/krb5.conf <<EOL
     dns_lookup_kdc = false
     ignore_acceptor_hostname = true
     rdns = false
+    noaddresses = TRUE
 [realms]
     PERCONATEST.COM = {
         kdc_ports = 88
@@ -25,9 +27,13 @@ EOL
 kdb5_util create -s -P password
 kadmin.local -q "addprinc -pw password root/admin"
 kadmin.local -q "addprinc -pw mongodb mongodb/${mongohost}"
+kadmin.local -q "addprinc -pw mongodb mongodb/${gateway_ip}"
 kadmin.local -q "addprinc -pw password1 pmm-test"
+
 kadmin.local -q "ktadd -k /tmp/mongodb.keytab mongodb/${mongohost}@PERCONATEST.COM"
+kadmin.local -q "ktadd -k /tmp/exporter.keytab mongodb/${gateway_ip}@PERCONATEST.COM"
+
 kadmin.local -q "ktadd -k /tmp/mongodb.keytab pmm-test@PERCONATEST.COM"
+kadmin.local -q "ktadd -k /tmp/exporter.keytab pmm-test@PERCONATEST.COM"
 
 krb5kdc -n
-
