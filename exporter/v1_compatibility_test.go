9d53c8244ebf58eef15a743905919def07f7ae08
Alex Demidoff <a@demidoff.me>
2026-06-16 23:09:46 +0300

    Update exporter/v1_compatibility_test.go

--- exporter/v1_compatibility_test.go
+++ exporter/v1_compatibility_test.go
@@ -232,7 +232,6 @@ func TestBalancerRunningValue(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 			got := balancerRunningValue(tt.inBalancerRound)
